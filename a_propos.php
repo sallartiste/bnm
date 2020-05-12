f85578374fd69411b2acf411d2c22cb67ac2fcb5
YoussefLutangu <65027658+YoussefLutangu@users.noreply.github.com>
2020-05-12 09:14:12 +0000

    Update a_propos.php

--- a_propos.php
+++ a_propos.php
@@ -7,24 +7,21 @@
 		<div class="content">
 		<section>
 			<article>
-		<P>Lorsque nous étions à la fin de nos études de Médecine, nous nous sommes décidés de réunir les différentes notes de cours
-		utilisées par nos prédécesseurs en vue d’une bonne préparation du stage interné d’une année qui sanctionne la fin des études de tout Médecin.</p>
-		<p>Nous nous sommes donc basés sur les notes qui ont été rédigées des mains de nos ainés ; pour ne pas nous encombrer de plusieurs notes de cours
-		en format papier, ce qui d’ailleurs poserait un sérieux problème de gestion du fait de l’espace qu’elles auraient occupé ; nous avons pris
+		<P>Lorsque j'étais à la fin de mes études de Médecine, je me suis décidé de réunir les différentes notes de cours 
+		utilisées par mes prédécesseurs en vue d’une bonne préparation du stage interné d’une année qui sanctionne la fin des études de tout Médecin.</p>
+		<p>Je me suis donc basés sur les notes qui ont été rédigées des mains de mes ainés ; pour ne pas m'encombrer de plusieurs notes de cours 
+		en papier, ce qui d’ailleurs poserait un sérieux problème de gestion du fait de l’espace qu’elles auraient occupé ; j'ai pris
 		l’ingénieuse idée de prendre en photo chaque page de tous les cours pour enfin en faire des documents sous format PDF ; cela fut un très grand
 		travail mais qui a néanmoins produit des résultats satisfaisants.</p>
-		<p>Juste avant notre stage interné plus précisément en Juin 2015, nous nous sommes rendus en vacances d’une semaine à Luozi dans le Kongo Central
-		comme nous en avions pris l’habitude depuis quelques années avec mon grand frère <em>SAMBA D.</em> résidant en Belgique ; étant consultant IT,
-		je lui ai parlé du projet que j’avais de réaliser une Bibliothèque en ligne et il m’a vivement encouragé de le faire.<br />
-		Au cours de notre stage interné d’une année soit de Juin 2015 à Juin 2016 à l’<strong>Hôpital Provincial Général de Référence de Kinshasa</strong>
-		qui comptait en son sein une centaine de Médecins Stagiaires, les documents manuscrits que nous avions mis sous format PDF ont connu un grand
-		succès et nous étions comptés parmi les grands distributeurs des documents électroniques que nous pouvions facilement lire à l’aide de nos téléphones
-		portables et que nous appelions communément <em>fichiers</em>.<br />
-		Après avoir fini nos études de Médecine, la demande de ces documents électroniques n’a cessé de croitre, ce qui a d’ailleurs consolidé notre idée
-		de réaliser cette Bibliothèque Numérique.</p>
-		<p>C'est dans ce même ordre d’idées que nous avons créé une rubrique consacrée aux travaux scientifiques ; nous restons donc ouverts à quiconque
+		<p>Au cours de mon stage interné d’une année soit de Juin 2015 à Juin 2016 à l’<em>Hôpital Provincial Général de Référence de Kinshasa</em> 
+		qui comptait en son sein une centaine de Médecins Stagiaires, les documents manuscrits que j'avais mis sous format PDF ont connu un grand 
+		succès et j'étais compté parmi les grands distributeurs des documents électroniques qui pouvaient facilement être lus à l’aide des téléphones 
+		portables et que nous appelions communément <em>fichiers</em>.</p>
+		<p>Après avoir fini mes études de Médecine, la demande de ces documents électroniques n’a cessé de croitre, ce qui a d’ailleurs consolidé mon idée 
+		de réaliser cette Bibliothèque Numérique, je me suis donc lancé à fond dans ce projet avec pratiquement aucune connaissance en developpement web.</p>
+		<p>C'est dans ce même ordre d’idées que nous avons créé une rubrique consacrée aux travaux scientifiques ; nous restons donc ouverts à quiconque 
 		souhaite mettre en ligne un travail scientifique.</p>
-		<p><em>Dr Youssef L.</em><p>
+		<p><em>Dr Youssef Lutangu</em><p>
 			</article>
 		</section>
 		</div>
