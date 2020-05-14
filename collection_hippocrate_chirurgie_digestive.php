4a5cfd39785b78d98804fdcb3a24aa5ddb976e49
YoussefLutangu <65027658+YoussefLutangu@users.noreply.github.com>
2020-05-14 11:42:39 +0000

    Update collection_hippocrate_chirurgie_digestive.php

--- collection_hippocrate_chirurgie_digestive.php
+++ collection_hippocrate_chirurgie_digestive.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style_1.css" />
-		<title>Collection Hippocrate Chirurgie Digestive</title>
-	</head>
-	<body>
-		<header>
-		<?php include("menus.php"); ?>
-		</header>
+	<?php include("inc/header.php"); ?>
 		<div id="titre_principal">
 			<h2>COLLECTION HIPPOCRATE CHIRURGIE DIGESTIVE</h2>
 		</div>
