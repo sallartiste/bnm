d47f67f1d43d28c43418fbf17bcac685c8f94e02
YoussefLutangu <65027658+YoussefLutangu@users.noreply.github.com>
2020-05-14 12:52:03 +0000

    Update guide_de_l_examen_clinique.php

--- guide_de_l_examen_clinique.php
+++ guide_de_l_examen_clinique.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style_1.css" />
-		<title>Guide de l'examen clinique</title>
-	</head>
-	<body>
-		<header>
-		<?php include("menus.php"); ?>
-		</header>
+	<?php include("inc/header.php"); ?>
 		<div id="titre_principal">
 			<h2>GUIDE DE L'EXAMEN CLINIQUE</h2>
 		</div>
