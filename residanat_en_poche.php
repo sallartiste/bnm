73a739eac837f4750bbc61ca62c91fa52558a786
YoussefLutangu <65027658+YoussefLutangu@users.noreply.github.com>
2020-05-14 13:48:31 +0000

    Update residanat_en_poche.php

--- residanat_en_poche.php
+++ residanat_en_poche.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style_1.css" />
-		<title>Résidanat en poche</title>
-	</head>
-	<body>
-			<header>
-			<?php include("menus.php"); ?>
-			</header>
+	<?php include("inc/header.php"); ?>
 			<div id="titre_principal">
 				<h2>RESIDANAT EN POCHE</h2>
 			</div>
