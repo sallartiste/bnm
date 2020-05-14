11c49c913872b3289e07851b82a8674e4e248877
YoussefLutangu <65027658+YoussefLutangu@users.noreply.github.com>
2020-05-14 11:55:35 +0000

    Update collection_hippocrate_medecine_du_travail.php

--- collection_hippocrate_medecine_du_travail.php
+++ collection_hippocrate_medecine_du_travail.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style_1.css" />
-		<title>Collection Hippocrate Médecine du travail</title>
-	</head>
-	<body>
-			<header>
-			<?php include("menus.php"); ?>
-			</header>
+	<?php include("inc/header.php"); ?>
 			<div id="titre_principal">
 				<h2>COLLECTION HIPPOCRATE MEDECINE DU TRAVAIL</h2>
 			</div>
