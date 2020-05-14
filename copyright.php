20b7f921d6dd2a87ae7bf228037d5c3ab7001c4e
sallartiste <sallartiste@gmail.com>
2020-05-14 14:21:46 +0200

    Update copyright.php

--- copyright.php
+++ copyright.php
@@ -1,4 +1,4 @@
 <div class="foot">
-	Copyright © 2020, BNM. Tous droits réservés<br />
+	Copyright © <?php echo date("Y");?>, BNM. Tous droits réservés<br />
 	<a href="a_propos.php">A propos</a> │ <a href="mailto:bibnumed@gmail.com" title="bibnumed@gmail.com">Contactez-nous</a> │ <a href="https://www.facebook.com/Biblioth%C3%A8que-Num%C3%A9rique-M%C3%A9dicale-518445355206235/" target="_blank"><img src="images/logo_facebook.png" title="Page facebook" alt="Page facebook" /></a></a>
 </div>
