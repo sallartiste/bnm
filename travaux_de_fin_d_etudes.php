39ea87ea759bab72735e0e68c958a13364bc945a
YoussefLutangu <65027658+YoussefLutangu@users.noreply.github.com>
2020-05-14 13:58:48 +0000

    Update travaux_de_fin_d_etudes.php

--- travaux_de_fin_d_etudes.php
+++ travaux_de_fin_d_etudes.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style_1.css" />
-		<title>Travaux de fin d'études</title>
-	</head>
-	<body>
-			<header>
-			<?php include("menus.php"); ?>
-			</header>
+	<?php include("inc/header.php"); ?>
 			<div id="titre_principal">
 				<h2>TRAVAUX DE FIN D'ETUDES</h2>
 			</div>
